0e5d8a1e09058cf02bb184c75975b22d3b14337a
trobonox <57040351+trobonox@users.noreply.github.com>
2022-06-28 22:40:18 +0200

    Finish implementation of css variable theme system

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="text-white bg-gray-800 min-h-screen w-full px-4 py-8">
-    A *default* layout
-    <slot />
+  <div class="default-layout min-h-screen mx-auto" :style="cssVars">
+
+    <div class="flex flex-row">
+        <slot />
+    </div>
   </div>
 </template>
 
@@ -11,7 +13,7 @@ import { dark } from "@/utils/themes.js"
 
 const store = useTauriStore().store
 const savedColors = await store.get("colors")
-const colorsUsed = ref({})
+const cssVars = ref({})
 
 onMounted(async () => {
     await updateColors()
@@ -23,7 +25,7 @@ const updateColors = async () => {
     if (!savedColors) {
         store.set("activeTheme", "dark")
 
-        colorsUsed.value = {
+        cssVars.value = {
           "--bg-primary": dark.bgPrimary,
           "--elevation-1": dark.elevation1,
           "--elevation-2": dark.elevation2,
@@ -38,8 +40,102 @@ const updateColors = async () => {
           "--text-buttons": dark.textButtons,
         }
     }
-    else { colorsUsed.value = savedColors }
+    else {
+        cssVars.value = {
+            "--bg-primary": savedColors.bgPrimary,
+            "--elevation-1": savedColors.elevation1,
+            "--elevation-2": savedColors.elevation2,
+            "--elevation-3": savedColors.elevation3,
+            "--accent": savedColors.accent,
+            "--accent-darker": savedColors.accentDarker,
+            "--text": savedColors.text,
+            "--text-dim-1": savedColors.textD1,
+            "--text-dim-2": savedColors.textD2,
+            "--text-dim-3": savedColors.textD3,
+            "--text-dim-4": savedColors.textD4,
+            "--text-buttons": savedColors.textButtons,
+        }
+    }
 }
-
-
 </script>
+
+<style>
+  .default-layout {
+    background-color: var(--bg-primary);
+    color: var(--text);
+  }
+
+  .bg-elevation-1 {
+    background-color: var(--elevation-1);
+  }
+
+  .bg-elevation-2 {
+    background-color: var(--elevation-2);
+  }
+
+  .bg-elevation-2-hover:hover {
+    background-color: var(--elevation-2);
+  }
+
+  .bg-elevation-3-hover:hover {
+    background-color: var(--elevation-3);
+  }
+
+  .bg-accent {
+    background-color: var(--accent);
+  }
+
+  .bg-accent:hover {
+    background-color: var(--accent-darker);
+  }
+
+  .border-accent {
+    border-color: var(--accent);
+  }
+
+  .border-accent-focus:focus {
+    border-color: var(--accent);
+  }
+
+  .text-accent {
+    color: var(--accent);
+  }
+
+  .text-accent-hover:hover {
+    color: var(--accent);
+  }
+
+  .text-accent-darker-hover:hover {
+    color: var(--accent-darker);
+  }
+
+  .text-normal {
+    color: var(--text);
+  }
+
+  .text-dim-1 {
+    color: var(--text-dim-1);
+  }
+
+  .text-dim-2 {
+    color: var(--text-dim-2);
+  }
+
+  .text-dim-3 {
+    color: var(--text-dim-3);
+  }
+
+  .text-dim-4 {
+    color: var(--text-dim-4);
+  }
+
+  .text-buttons {
+    color: var(--text-buttons);
+  }
+
+  .text-no-overflow {
+    overflow-wrap: break-word;
+    white-space: normal;
+    overflow: hidden;
+  }
+</style>
