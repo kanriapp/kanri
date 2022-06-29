9ec8dffae2c2051238e9857d02aaf885cba3ea8b
trobonox <57040351+trobonox@users.noreply.github.com>
2022-06-29 22:12:14 +0200

    Add empty settings page

---
+++ pages/settings.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="mt-8">
+        <h1 class="font-bold text-4xl">Settings</h1>
+        <h2 class="ml-1 text-dim-3">Change the look and behaviour of the app.</h2>
+    </div>
+</template>
