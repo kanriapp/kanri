cd13e2a27d021982fa70793983d960e957624eab
trobonox <57040351+trobonox@users.noreply.github.com>
2022-07-02 20:26:52 +0200

    Set up basic drag and drop test

--- pages/kanban/[id].vue
+++ pages/kanban/[id].vue
@@ -1,19 +1,48 @@
 <template>
-    <div>
-         {{ $route.params.id }} -
-         {{ board.title }}
+    <div class="flex flex-col">
+        <h1 class="my-4 text-2xl font-bold">
+             {{ $route.params.id }} -
+             {{ board.title }}
+        </h1>
+        <Container
+            @drop="onDrop"
+            group-name="columns"
+            :orientation="'horizontal'"
+            class="flex-row gap-4"
+            :get-child-payload="getChildPayload"
+        >
+            <Draggable>
+                <div class="flex flex-row">
+                    <div class="p-4 bg-elevation-1">Box 1</div>
+                </div>
+            </Draggable>
+            <Draggable>
+                <div class="flex flex-row">
+                    <div class="p-4 bg-elevation-1">Box 2</div>
+                </div>
+            </Draggable>
+        </Container>
+
+
     </div>
 </template>
 
 <script setup>
 import { useTauriStore } from "@/stores/tauriStore"
+import { Container, Draggable } from "vue3-smooth-dnd"
+
 const store = useTauriStore().store
 const route = useRoute()
 const boards = await store.get("boards")
 const board = boards[route.params.id]
-console.log(board)
+
+const onDrop = () => {
+    console.log("pog")
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.smooth-dnd-container.horizontal {
+  display: flex;
+}
 </style>
