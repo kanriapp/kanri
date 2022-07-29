11ffa288d5cb2a2c205256a7a96d29f5f09e97c9
trobonox <57040351+trobonox@users.noreply.github.com>
2022-07-29 16:14:39 +0200

    Strip custom logic from modal component to make it barebones

--- components/Modal.vue
+++ components/Modal.vue
@@ -5,42 +5,9 @@
             class="modal inset-0 z-50 flex h-screen w-screen flex-col items-center justify-center bg-zinc-800 bg-opacity-40 bg-clip-padding backdrop-blur-xl backdrop-filter"
         >
             <div
-                class="bg-elevation-1 modal-body flex h-4/5 w-2/5 flex-col items-start rounded-md py-4 pl-8 pr-6 shadow-lg"
+                class="bg-elevation-1 min-h-content min-w-content rounded-md py-4 pl-8 pr-6 shadow-lg"
             >
-                <div class="flex w-full flex-row items-center justify-between">
-                    <h1
-                        @click="enableTitleEditing"
-                        v-if="!titleEditing"
-                        :v-model="modalTitle"
-                        class="pointer-events-auto pr-5 text-2xl font-bold"
-                    >
-                        {{ modalTitle || title }}
-                    </h1>
-                    <input
-                        @blur="setTitleRemote()"
-                        @keypress.enter="setTitleRemote()"
-                        v-if="titleEditing"
-                        ref="titleInput"
-                        type="text"
-                        v-model="modalTitle"
-                        class="bg-elevation-2 text-normal border-accent-focus pointer-events-auto text-xl focus:border-2 focus:border-dotted focus:outline-none"
-                    />
-                    <div @click="$emit('closeModal')" class="p-1">
-                        <svg
-                            class="text-dim-4 text-accent-hover h-6 w-6"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                            fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                            ></path>
-                        </svg>
-                    </div>
-                </div>
-                <slot name="content"></slot>
+                <slot name="content" class="p-4"></slot>
             </div>
         </div>
     </transition>
