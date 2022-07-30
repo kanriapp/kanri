d3749151f675ae0063260fdeecd92e893aa8d3fa
trobonox <57040351+trobonox@users.noreply.github.com>
2022-07-30 22:29:59 +0200

    Fix default layout for overflow situations

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="default-layout min-h-screen mx-auto" :style="cssVars">
+    <div class="default-layout min-h-screen min-w-full max-w-max mx-auto" :style="cssVars">
 
-    <div class="flex flex-row">
-        <Sidebar class="sticky top-0" />
-        <slot />
+        <div class="flex flex-row">
+            <Sidebar class="sticky top-0" />
+            <slot />
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
@@ -25,18 +25,18 @@ const updateColors = async () => {
         store.set("activeTheme", "dark")
 
         cssVars.value = {
-          "--bg-primary": dark.bgPrimary,
-          "--elevation-1": dark.elevation1,
-          "--elevation-2": dark.elevation2,
-          "--elevation-3": dark.elevation3,
-          "--accent": dark.accent,
-          "--accent-darker": dark.accentDarker,
-          "--text": dark.text,
-          "--text-dim-1": dark.textD1,
-          "--text-dim-2": dark.textD2,
-          "--text-dim-3": dark.textD3,
-          "--text-dim-4": dark.textD4,
-          "--text-buttons": dark.textButtons,
+            "--bg-primary": dark.bgPrimary,
+            "--elevation-1": dark.elevation1,
+            "--elevation-2": dark.elevation2,
+            "--elevation-3": dark.elevation3,
+            "--accent": dark.accent,
+            "--accent-darker": dark.accentDarker,
+            "--text": dark.text,
+            "--text-dim-1": dark.textD1,
+            "--text-dim-2": dark.textD2,
+            "--text-dim-3": dark.textD3,
+            "--text-dim-4": dark.textD4,
+            "--text-buttons": dark.textButtons,
         }
     }
     else {
@@ -59,82 +59,82 @@ const updateColors = async () => {
 </script>
 
 <style>
-  .default-layout {
-    background-color: var(--bg-primary);
-    color: var(--text);
-  }
+    .default-layout {
+        background-color: var(--bg-primary);
+        color: var(--text);
+    }
 
-  .bg-elevation-1 {
-    background-color: var(--elevation-1);
-  }
+    .bg-elevation-1 {
+        background-color: var(--elevation-1);
+    }
 
-  .bg-elevation-2 {
-    background-color: var(--elevation-2);
-  }
+    .bg-elevation-2 {
+        background-color: var(--elevation-2);
+    }
 
-  .bg-elevation-2-hover:hover {
-    background-color: var(--elevation-2);
-  }
+    .bg-elevation-2-hover:hover {
+        background-color: var(--elevation-2);
+    }
 
-  .bg-elevation-3-hover:hover {
-    background-color: var(--elevation-3);
-  }
+    .bg-elevation-3-hover:hover {
+        background-color: var(--elevation-3);
+    }
 
-  .bg-accent {
-    background-color: var(--accent);
-  }
+    .bg-accent {
+        background-color: var(--accent);
+    }
 
-  .bg-accent:hover {
-    background-color: var(--accent-darker);
-  }
+    .bg-accent:hover {
+        background-color: var(--accent-darker);
+    }
 
-  .border-accent {
-    border-color: var(--accent);
-  }
+    .border-accent {
+        border-color: var(--accent);
+    }
 
-  .border-accent-focus:focus {
-    border-color: var(--accent);
-  }
+    .border-accent-focus:focus {
+        border-color: var(--accent);
+    }
 
-  .text-accent {
-    color: var(--accent);
-  }
+    .text-accent {
+        color: var(--accent);
+    }
 
-  .text-accent-hover:hover {
-    color: var(--accent);
-  }
+    .text-accent-hover:hover {
+        color: var(--accent);
+    }
 
-  .text-accent-darker-hover:hover {
-    color: var(--accent-darker);
-  }
+    .text-accent-darker-hover:hover {
+        color: var(--accent-darker);
+    }
 
-  .text-normal {
-    color: var(--text);
-  }
+    .text-normal {
+        color: var(--text);
+    }
 
-  .text-dim-1 {
-    color: var(--text-dim-1);
-  }
+    .text-dim-1 {
+        color: var(--text-dim-1);
+    }
 
-  .text-dim-2 {
-    color: var(--text-dim-2);
-  }
+    .text-dim-2 {
+        color: var(--text-dim-2);
+    }
 
-  .text-dim-3 {
-    color: var(--text-dim-3);
-  }
+    .text-dim-3 {
+        color: var(--text-dim-3);
+    }
 
-  .text-dim-4 {
-    color: var(--text-dim-4);
-  }
+    .text-dim-4 {
+        color: var(--text-dim-4);
+    }
 
-  .text-buttons {
-    color: var(--text-buttons);
-  }
+    .text-buttons {
+        color: var(--text-buttons);
+    }
 
-  .text-no-overflow {
-    overflow-wrap: break-word;
-    white-space: normal;
-    overflow: hidden;
-  }
+    .text-no-overflow {
+        overflow-wrap: break-word;
+        white-space: normal;
+        overflow: hidden;
+    }
 </style>
