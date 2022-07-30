9dfee9378bd7c6ee16acdf35ea2fa91f23ec6f8e
trobonox <57040351+trobonox@users.noreply.github.com>
2022-07-30 21:48:21 +0200

    Start rewriting components to TS and composition API

--- components/kanban/Column.vue
+++ components/kanban/Column.vue
@@ -1,25 +1,23 @@
 <template>
     <div class="bg-elevation-1 flex w-64 flex-col rounded-md p-2 shadow-lg">
         <ModalKanban v-show="modalVisible" ref="modal" @setCardTitle="setCardTitle"
-            @setCardDescription="setCardDescription" @closeModal="closeModal" />
+            @setCardDescription="setCardDescription" @closeModal="modalVisible = false" />
 
-        <div class="flex flex-row items-start justify-between gap-4">
+        <div id="board-title" class="flex flex-row items-start justify-between gap-4">
             <h1 v-if="!titleEditing" @click="titleEditing = true; $nextTick(() => $refs.titleInput.focus());"
                 class="text-no-overflow ml-1 text-lg font-bold">
                 {{ titleNew }}
             </h1>
+
             <input ref="titleInput" v-if="titleEditing" type="text" v-model="titleNew"
                 class="bg-elevation-2 border-accent text-no-overflow mr-2 w-full rounded-sm border-2 border-dotted px-2 text-lg outline-none"
                 @blur="titleEditing = false; updateStorage();"
                 @keypress.enter="titleEditing = false; updateStorage();" />
-            <svg class="text-dim-4 text-accent-hover mt-2 h-4 w-4 flex-shrink-0 flex-grow-0 cursor-pointer"
-                fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
-                @click="$emit('removeColumn', id)">
-                <path fill-rule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"></path>
-            </svg>
+
+            <XIcon class="mt-2 h-4 w-4 text-dim-4 flex-shrink-0 flex-grow-0 text-accent-hover cursor-pointer"
+                @click="$emit('removeColumn', id)" />
         </div>
+
         <Container group-name="cards" :get-child-payload="getChildPayload"
             class="max-h-65vh custom-scrollbar mt-2 overflow-y-auto rounded-sm" @drop="onDrop"
             :drag-handle-selector="dragHandleSelector">
@@ -40,6 +38,7 @@
                 </div>
             </Draggable>
         </Container>
+
         <div v-if="cardAddMode" class="mt-2 flex flex-col">
             <textarea id="newCardInput" ref="newCardInput" type="text" placeholder="Enter a card title..."
                 v-model="newCardName"
@@ -59,19 +58,103 @@
         <div v-if="!cardAddMode"
             class="text-dim-1 bg-elevation-3-hover mt-2 flex cursor-pointer flex-row gap-1 rounded-md py-1 font-medium"
             @click="cardAddMode = !cardAddMode; $nextTick(() => $refs.newCardInput.focus());">
+
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
             </svg>
+
             <h2>Add Card</h2>
         </div>
+
+
     </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import { Container, Draggable } from "vue3-smooth-dnd";
+import { XIcon } from "@heroicons/vue/solid"
+import emitter from "@/utils/emitter";
+import { applyDrag } from "@/utils/drag-n-drop"
 
-export default {
+const props = defineProps<{
+    id: string,
+    title: string,
+    cardsList: Array<Object>
+}>()
+
+const emit = defineEmits(["updateStorage"])
+
+const cards = ref(props.cardsList)
+const newCardName = ref("")
+const cardAddMode = ref(false)
+const titleNew = ref(props.title)
+const titleEditing = ref(false)
+const modalVisible = ref(false)
+const draggingEnabled = ref(true)
+
+const dragHandleSelector = computed(() => {
+    return draggingEnabled ? "" : "dragging_disabled"
+})
+
+const onDrop = (dropResult) => {
+    cards.value = applyDrag(cards.value, dropResult);
+    updateStorage();
+}
+
+const getChildPayload = (index: number) => {
+    return cards.value[index];
+}
+
+const addCard = (event) => {
+    if (
+        (event.relatedTarget && event.relatedTarget.id === "submitButton") ||
+        event instanceof KeyboardEvent
+    ) {
+        cards.value[cards.value.length] = { name: newCardName.value }
+    }
+
+    newCardName.value = "";
+    cardAddMode.value = false;
+    //updateStorage();
+}
+
+const removeCard = (index: number) => {
+    cards.value.splice(index, 1);
+}
+
+const setCardTitle = (cardIndex: number, title: string) => {
+    //cards.value[cardIndex]["title"] = title;
+}
+
+const setCardDescription = () => {
+
+}
+
+const openModal = () => {
+
+}
+
+const openModalFromChild = () => {
+
+}
+
+const closeModal = () => {
+
+}
+
+const updateStorage = () => {
+    const column = {
+        id: props.id,
+        title: titleNew,
+        cards: cards,
+    };
+
+    emit("updateStorage", column);
+}
+
+
+/*export default {
     components: { Container, Draggable},
     props: {
         id: {
@@ -122,49 +205,7 @@ export default {
     },
 
     methods: {
-        onDrop(dropResult) {
-            this.cards = this.applyDrag(this.cards, dropResult);
-            this.updateStorage();
-        },
 
-        getChildPayload(index) {
-            return this.cards[index];
-        },
-
-        applyDrag(arr, dragResult) {
-            const { removedIndex, addedIndex, payload } = dragResult;
-            if (removedIndex === null && addedIndex === null) return arr;
-
-            const result = [...arr];
-            let itemToAdd = payload;
-
-            if (removedIndex !== null) {
-                itemToAdd = result.splice(removedIndex, 1)[0];
-            }
-
-            if (addedIndex !== null) {
-                result.splice(addedIndex, 0, itemToAdd);
-            }
-
-            return result;
-        },
-
-        addCard(event) {
-            if (
-                (event.relatedTarget && event.relatedTarget.id === "submitButton") ||
-                event instanceof KeyboardEvent
-            ) {
-                this.$set(
-                    this.cards,
-                    this.cards.length,
-                    JSON.parse(JSON.stringify({ name: this.newCardName }))
-                );
-            }
-
-            this.newCardName = "";
-            this.cardAddMode = false;
-            this.updateStorage();
-        },
 
         removeCard(index) {
             this.$delete(this.cards, index);
@@ -186,7 +227,8 @@ export default {
             const cardTitle = this.cards[cardIndex].name;
             const cardDescription = this.cards[cardIndex].description;
 
-            this.$refs.modal.initModal(cardIndex, cardTitle, cardDescription);
+            //this.$refs.modal.initModal(cardIndex, cardTitle, cardDescription);
+            console.log(this.$refs)
             this.modalVisible = true;
             this.draggingEnabled = false;
             this.$emit("modalOpen");
@@ -216,4 +258,5 @@ export default {
         },
     },
 };
+*/
 </script>
--- components/modal/Kanban.vue
+++ components/modal/Kanban.vue
@@ -1,59 +1,52 @@
 <template>
-  <Modal
-    :title="title"
-    ref="barebonesModal"
-    :titleEditingAllowed="true"
-    @setTitle="setCardTitle"
-    @closeModal="$emit('closeModal')"
-  >
-    <template v-slot:content>
-      <span class="text-md text-dim-3 mb-6"
-        >Edit all the things about your card!</span
-      >
-      <label for="cardDescription" class="text-lg font-semibold"
-        >Card Description</label
-      >
-      <textarea
-        @blur="updateDescription"
-        @keypress.enter="updateDescription"
-        v-model="description"
-        name="cardDescription"
-        id="cardDescription"
-        cols="6"
-        rows="70"
-        placeholder="Enter a detailed description of your card here..."
-        class="bg-elevation-2 border-accent-focus pointer-events-auto mt-2 h-40 w-11/12 resize-none rounded-md p-2 shadow-lg focus:border-2 focus:border-dotted focus:outline-none"
-      >
-      </textarea>
-    </template>
-  </Modal>
+    <Modal
+        :title="title"
+        ref="barebonesModal"
+        @closeModal="$emit('closeModal')"
+    >
+        <template v-slot:content>
+            <div class="div flex flex-col">
+                <span class="text-md text-dim-3 mb-6">
+                    Edit all the things about your card!
+                </span>
+
+                <label for="cardDescription" class="text-lg font-semibold">
+                    Card Description
+                </label>
+                <textarea
+                    @blur="updateDescription"
+                    @keypress.enter="updateDescription"
+                    v-model="description"
+                    name="cardDescription"
+                    id="cardDescription"
+                    cols="6"
+                    rows="70"
+                    placeholder="Enter a detailed description of your card here..."
+                    class="bg-elevation-2 border-accent-focus pointer-events-auto mt-2 h-40 w-11/12 resize-none rounded-md p-2 shadow-lg focus:border-2 focus:border-dotted focus:outline-none"
+                >
+                </textarea>
+            </div>
+        </template>
+    </Modal>
 </template>
 
-<script>
-import Modal from "~/components/Modal.vue";
-export default {
-  components: { Modal },
-  data() {
-    return {
-      cardId: "",
-      title: "",
-      description: "",
-    };
-  },
-  methods: {
-    initModal(cardID, title, description) {
-      this.cardId = cardID;
-      this.title = title;
-      this.description = description;
-      this.$refs.barebonesModal.setTitle(title);
-    },
-    updateDescription() {
-      this.$emit("setCardDescription", this.cardId, this.description);
-    },
-    setCardTitle(title) {
-      this.title = title;
-      this.$emit("setCardTitle", this.cardId, title);
-    },
-  },
-};
+<script setup lang="ts">
+const cardID = ref(0)
+const title = ref("")
+const description = ref("")
+
+const initModal = (cardIdParam: number, titleParam: string, descriptionParam?: string) => {
+    cardID.value = cardIdParam;
+    title.value = titleParam;
+    description.value = descriptionParam || "";
+}
+
+const updateDescription = () => {
+    this.$emit("setCardDescription", cardID.value, description.value);
+}
+
+const setCardTitle = (titleParam: string) => {
+    title.value = titleParam;
+    this.$emit("setCardTitle", cardID.value, title.value);
+}
 </script>
