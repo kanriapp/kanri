1e3e7dea60204aa66d35e8cea82cd6d56b90aea6
trobonox <57040351+trobonox@users.noreply.github.com>
2022-07-30 21:44:55 +0200

    Move applyDrag func to extra util for reusability

---
+++ utils/drag-n-drop.js
@@ -0,0 +1,17 @@
+export function applyDrag(arr, dragResult) {
+    const { removedIndex, addedIndex, payload } = dragResult;
+    if (removedIndex === null && addedIndex === null) return arr;
+
+    const result = [...arr];
+    let itemToAdd = payload;
+
+    if (removedIndex !== null) {
+        itemToAdd = result.splice(removedIndex, 1)[0];
+    }
+
+    if (addedIndex !== null) {
+        result.splice(addedIndex, 0, itemToAdd);
+    }
+
+    return result;
+}
