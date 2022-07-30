2746b9490abeb9e0be88d19fb7f8449c1995ba84
trobonox <57040351+trobonox@users.noreply.github.com>
2022-07-30 21:44:28 +0200

    Add typescript interfaces for custom kanban objects

---
+++ types/kanban-types.d.ts
@@ -0,0 +1,22 @@
+export declare interface Board {
+    id: string,
+    title: string,
+    columns: Array<any>
+}
+
+export declare interface Column {
+    id?: string,
+    cards: Array<any>
+}
+
+export declare interface Card {
+    id?: string,
+    name: string,
+    description?: string
+}
+
+export default {
+    Board,
+    Column,
+    Card
+}
