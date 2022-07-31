99ac407f21111e51fdd8150630010d0e77f8fa3d
trobonox <57040351+trobonox@users.noreply.github.com>
2022-07-31 23:02:30 +0200

    Adjust board preview component

--- pages/index.vue
+++ pages/index.vue
@@ -27,7 +27,7 @@
                     :to="'/kanban/' + index"
                 >
                     <img
-                        class="w-56 rounded-t-md filter group-hover:contrast-50"
+                        class="w-56 rounded-t-md filter group-hover:brightness-75"
                         src="https://images.unsplash.com/photo-1656751609190-e0168efca2da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
                         alt="pog"
                     />
