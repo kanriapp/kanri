fe90a2de26fc3ea5e6d5dff2b3f24b27c1ab0e15
Trobo <57040351+trobonox@users.noreply.github.com>
2022-09-17 11:16:28 +0200

    styles: redesign sidebar with new logo

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,39 +1,33 @@
 <template>
     <nav
-        class="flex flex-col items-center justify-between h-screen px-4 pb-6 pt-4 mr-8 bg-elevation-1 sticky left-0 top-0"
+        class="flex flex-col items-center justify-between h-screen px-2 pb-6 pt-4 mr-8 bg-elevation-1 sticky left-0 top-0"
         :class="zIndexDown ? '' : 'z-50'"
     >
         <ModalHelp v-show="modalVisible" ref="modal" @closeModal="modalVisible = false" />
         <section id="items-top" class="flex flex-col items-center gap-4">
             <!-- temporary logo placeholder -->
-            <div id="logo" class="flex flex-row p-2 mb-2 bg-elevation-2 rounded-md">
-                <div class="h-9 w-4 mr-1 bg-violet-600 rounded-sm"></div>
-                <div class="flex flex-col gap-1">
-                    <div class="h-4 w-4 bg-teal-500 rounded-sm"></div>
-                    <div class="h-4 w-4 bg-blue-500 rounded-sm"></div>
-                </div>
+            <div id="logo" class="flex flex-row p-1 rounded-md">
+                <IconKanri class=" w-10 h-10" />
             </div>
             <nuxt-link to="/">
-                <div class="p-2 bg-elevation-2 bg-elevation-3-hover cursor-pointer rounded-md">
+                <div class="p-2 bg-elevation-2-hover rounded-md">
                     <HomeIcon class="h-6 w-6" />
                 </div>
             </nuxt-link>
             <button
                 v-if="showAddButton"
-                class="rounded-md bg-accent text-buttons p-2"
+                class="p-2 bg-elevation-2-hover rounded-md"
                 @click="emitter.emit('createBoard')"
             >
-                <PlusIcon class="h-6 w-6" />
+                <PlusIcon class="h-6 w-6 text-accent" />
             </button>
         </section>
         <section id="icons-bottom" class="flex flex-col items-center gap-4">
-            <button @click="modalVisible = true">
-                <div class="p-2 bg-elevation-2 bg-elevation-3-hover cursor-pointer rounded-md">
-                    <QuestionMarkCircleIcon class="h-6 w-6" />
-                </div>
+            <button @click="modalVisible = true" class="p-2 bg-elevation-2-hover rounded-md">
+                <QuestionMarkCircleIcon class="h-6 w-6" />
             </button>
             <nuxt-link to="/settings">
-                <div class="p-2 bg-elevation-2 bg-elevation-3-hover cursor-pointer rounded-md">
+                <div class="p-2 bg-elevation-2-hover rounded-md">
                     <CogIcon class="h-6 w-6" />
                 </div>
             </nuxt-link>
