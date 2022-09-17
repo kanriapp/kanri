978f9b4c07ae37ef07b081e659fa5cd617e29fd8
Trobo <57040351+trobonox@users.noreply.github.com>
2022-09-17 11:40:44 +0200

    feat: add global css and disable overscroll (bouncy scroll)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     ssr: false,
     modules: ['@nuxtjs/tailwindcss'],
     buildModules: ['@pinia/nuxt', 'floating-vue/nuxt'],
-    css: ['@/assets/css/scrollbars.css']
+    css: ['@/assets/css/scrollbars.css', '@/assets/css/global.css']
 })
