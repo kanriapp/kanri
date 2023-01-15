d8cb393d874903d5eda7dc2bd6255b573f792703
Trobo <57040351+trobonox@users.noreply.github.com>
2023-01-15 09:30:06 +0100

    fix: tauri plugin store init

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,12 +3,11 @@
   windows_subsystem = "windows"
 )]
 
-use tauri_plugin_store::PluginBuilder;
 use tauri_plugin_autostart::MacosLauncher;
 
 fn main() {
   tauri::Builder::default()
-    .plugin(PluginBuilder::default().build())
+    .plugin(tauri_plugin_store::Builder::default().build())
     .plugin(tauri_plugin_persisted_scope::init())
     .plugin(tauri_plugin_autostart::init(
       MacosLauncher::LaunchAgent,
