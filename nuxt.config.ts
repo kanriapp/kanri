55d106da1908cee61b7d37d7ef33f5134eeaa19d
trobonox <57040351+trobonox@users.noreply.github.com>
2023-01-29 19:24:37 +0100

    feat: add automated license report generation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,6 @@
+import license from "rollup-plugin-license";
+import "node:path"
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     ssr: false,
@@ -5,4 +8,15 @@ export default defineNuxtConfig({
     css: ['@/assets/css/scrollbars.css', '@/assets/css/global.css'],
     telemetry: false,
     app: { pageTransition: { name: 'page', mode: 'out-in' } },
+    vite: {
+        //@ts-ignore
+        plugins: [
+            license({
+                thirdParty: {
+                    output: "LICENSES_3RD_PARTY.txt",
+                    includePrivate: true,
+                },
+            }),
+        ],
+    },
 })
