c72d2bdd97a0024fe9d00f313a16fbc61acf5db7
trobonox <57040351+trobonox@users.noreply.github.com>
2023-02-21 23:05:44 +0100

    feat: initial full kanri json import with zod

--- pages/index.vue
+++ pages/index.vue
@@ -39,6 +39,7 @@
     </section>
 
     <section
+      v-if="!(boards.length === 0 && loading === false)"
       id="filters"
       class="mt-2"
     >
@@ -85,13 +86,28 @@
     <main id="boards">
       <div
         v-if="boards.length === 0 && loading === false"
-        class="items-left bg-elevation-1 mt-2 flex w-fit flex-col justify-center rounded-md p-2"
+        class="items-left mt-2 flex w-fit flex-col justify-center rounded-md p-2"
       >
         <h3 class="text-xl font-bold">
           So empty here!
         </h3>
         <span>Create a board to get started with tracking your tasks better.</span>
         <IconArrow />
+
+        <h3 class="mt-8 mb-0.5 text-xl font-bold">
+          Have some data already?
+        </h3>
+        <p class="mb-4">
+          You can import data from another Kanri instance or Trello below:
+        </p>
+        <div class="flex flex-row gap-4">
+          <button @click="importFromKanri" class="bg-elevation-1 bg-elevation-2-hover border-accent cursor-pointer rounded-md border border-dotted p-4 font-semibold">
+            Import from Kanri
+          </button>
+          <button @click="importFromTrello" class="bg-elevation-1 bg-elevation-2-hover border-accent cursor-pointer rounded-md border border-dotted p-4 font-semibold">
+            Import from Trello
+          </button>
+        </div>
       </div>
 
       <div
@@ -163,8 +179,14 @@ import emitter from "@/utils/emitter";
 import { useTauriStore } from "@/stores/tauriStore";
 import { generateUniqueID } from "@/utils/idGenerator.js";
 import type { Board } from "@/types/kanban-types";
+import { kanriJsonSchema } from "@/types/json-schemas"
 
 import { Ref } from "vue";
+
+import { z, ZodError } from "zod";
+
+import { readTextFile } from '@tauri-apps/api/fs';
+import { open, message } from '@tauri-apps/api/dialog';
 
 import { EllipsisHorizontalIcon } from "@heroicons/vue/24/solid";
 import { FunnelIcon } from "@heroicons/vue/24/outline";
@@ -321,6 +343,60 @@ const sortBoardsByEditDate = () => {
     });
     store.set("boardSortingOption", "edited");
     sortingOptionText.value = "Sort by edit date";
+}
+
+const importFromKanri = async () => {
+    const selected = await open({
+        multiple: false,
+        filters: [{
+            name: 'JSON File',
+            extensions: ['json']
+        }]
+    });
+
+    if (selected === null) return;
+
+    const textFile = await readTextFile(selected as string);
+    if (!textFile) return;
+
+    let parsedJson = null;
+    try {
+        parsedJson = JSON.parse(textFile);
+    }
+    catch (error) {
+        console.error("Could not parse imported JSON;", error);
+        await message('Could not load JSON file! Please check the file is correct.', { title: 'Kanri', type: 'error' });
+    }
+    if (parsedJson === null) return;
+
+    let zodParsed = null;
+    try {
+        zodParsed = kanriJsonSchema.parse(parsedJson);
+    }
+    catch (error) {
+        console.error(error);
+        //@ts-ignore
+        if (error.issues[0].code === "invalid_type" && error.issues[0].path[0] === "boards" && error.issues[0].received === "null") {
+            return await message('Cannot load files with no boards. Please import a file with at least one board.', { title: 'Kanri', type: 'error' });
+        }
+
+        await message('Could not load JSON file! Please check the file is formatted correctly and not from an old version of Kanri.', { title: 'Kanri', type: 'error' });
+    }
+    if (zodParsed === null) return;
+
+    store.set("boards", zodParsed.boards);
+    store.set("colors", zodParsed.colors);
+    store.set("activeTheme", zodParsed.activeTheme);
+    if (zodParsed.columnZoomLevel) {
+        store.set("columnZoomLevel", zodParsed.columnZoomLevel);
+    }
+
+    // Manual refresh
+    boards.value = (await store.get("boards")) || [];
+}
+
+const importFromTrello = () => {
+    // TODO
 }
 </script>
 
---
+++ types/json-schemas.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+
+export const kanriJsonSchema = z.object({
+    boards: z.array(z.object({
+        id: z.string(),
+        title: z.string(),
+        lastEdited: z.string(),
+        columns: z.array(z.object({
+            id: z.string(),
+            title: z.string(),
+            cards: z.array(z.object({
+                name: z.string(),
+                description: z.string(),
+                color: z.string().optional()
+            }))
+        })),
+        background: z.object({
+            blur: z.string(),
+            brightness: z.string(),
+            src: z.string()
+        }).optional()
+    })),
+    activeTheme: z.string(),
+    colors: z.object({
+        accent: z.string(),
+        accentDarker: z.string(),
+        bgPrimary: z.string(),
+        elevation1: z.string(),
+        elevation2: z.string(),
+        elevation3: z.string(),
+        text: z.string(),
+        textButtons: z.string(),
+        textD1: z.string(),
+        textD2: z.string(),
+        textD3: z.string(),
+        textD4: z.string()
+    }),
+    columnZoomLevel: z.string().optional()
+});
