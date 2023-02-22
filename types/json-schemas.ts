74c4e4ce8a321f9bc6b858b3065583c07d4b7023
Trobo <57040351+trobonox@users.noreply.github.com>
2023-02-22 10:31:40 +0100

    refactor: split up schema into multiple vars

--- types/json-schemas.ts
+++ types/json-schemas.ts
@@ -4,40 +4,51 @@
 
 import { z } from "zod";
 
+const kanriThemeSchema = z.object({
+    accent: z.string(),
+    accentDarker: z.string(),
+    bgPrimary: z.string(),
+    elevation1: z.string(),
+    elevation2: z.string(),
+    elevation3: z.string(),
+    text: z.string(),
+    textButtons: z.string(),
+    textD1: z.string(),
+    textD2: z.string(),
+    textD3: z.string(),
+    textD4: z.string()
+});
+
+const kanriCardSchema = z.object({
+    id: z.string().optional(),
+    name: z.string(),
+    description: z.string().optional(),
+    color: z.string().optional()
+});
+
+const kanriColumnSchema = z.object({
+    id: z.string(),
+    title: z.string(),
+    cards: z.array(kanriCardSchema)
+});
+
+const kanriBoardBackgroundSchema = z.object({
+    blur: z.string(),
+    brightness: z.string(),
+    src: z.string()
+}).optional()
+
+const kanriBoardSchema =z.object({
+    id: z.string(),
+    title: z.string(),
+    lastEdited: z.string(),
+    columns: z.array(kanriColumnSchema),
+    background: kanriBoardBackgroundSchema
+})
+
 export const kanriJsonSchema = z.object({
-    boards: z.array(z.object({
-        id: z.string(),
-        title: z.string(),
-        lastEdited: z.string(),
-        columns: z.array(z.object({
-            id: z.string(),
-            title: z.string(),
-            cards: z.array(z.object({
-                name: z.string(),
-                description: z.string(),
-                color: z.string().optional()
-            }))
-        })),
-        background: z.object({
-            blur: z.string(),
-            brightness: z.string(),
-            src: z.string()
-        }).optional()
-    })),
+    boards: z.array(kanriBoardSchema),
     activeTheme: z.string(),
-    colors: z.object({
-        accent: z.string(),
-        accentDarker: z.string(),
-        bgPrimary: z.string(),
-        elevation1: z.string(),
-        elevation2: z.string(),
-        elevation3: z.string(),
-        text: z.string(),
-        textButtons: z.string(),
-        textD1: z.string(),
-        textD2: z.string(),
-        textD3: z.string(),
-        textD4: z.string()
-    }),
+    colors: kanriThemeSchema,
     columnZoomLevel: z.string().optional()
 });
