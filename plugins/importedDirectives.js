dd3be2b9f16b2748f0404a22fd1411d96ef48883
trobonox <57040351+trobonox@users.noreply.github.com>
2023-09-06 19:05:00 +0200

    feat: allow dragging to scroll board horizontally

--- plugins/importedDirectives.js
+++ plugins/importedDirectives.js
@@ -5,7 +5,9 @@
 import { defineNuxtPlugin } from '#app'
 import FloatingVue from 'floating-vue'
 import 'floating-vue/dist/style.css'
+import VueDragscroll from "vue-dragscroll";
 
 export default defineNuxtPlugin(({ vueApp }) => {
     vueApp.use(FloatingVue)
+    vueApp.use(VueDragscroll);
 })
