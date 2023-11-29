71ea25564abc1c0d6f5d06c6ecff13673ba5772e
trobonox <57040351+trobonox@users.noreply.github.com>
2023-11-29 14:23:05 +0100

    chore: add another feature to changelog

--- components/modal/Changelog.vue
+++ components/modal/Changelog.vue
@@ -24,6 +24,7 @@
           - Fixed contrast issues when double clicking on a card's text to quickly edit it <br>
           - Import checklist items from Trello™ <br>
           - Feature to disable all animations from settings <br>
+          - Clicking on the Kanri icon in the sidebar acts as a home button <br>
           - Other miscellaneous fixes and improvements (full changelog available in the GitHub release notes)
         </p>
         <div class="mt-6 flex justify-between">
