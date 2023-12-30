2deffa16598245577682cec65c8f71c7d2f9ef74
trobonox <57040351+trobonox@users.noreply.github.com>
2023-12-30 21:24:09 +0200

    chore: fix reuse compliance

--- utils/objects.ts
+++ utils/objects.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022-2023 trobonox <hello@trobo.tech>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 export function findObjectById<Type> (objectArray: Array<Type>, objectId: string) {
     return objectArray.filter((object) => {
         // @ts-ignore
