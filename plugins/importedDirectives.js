d05968516cec746d5d001d4c10a16b78770d9e07
trobonox <57040351+trobonox@users.noreply.github.com>
2023-12-30 09:30:27 +0200

    chore: add v-calendar for due dates

--- plugins/importedDirectives.js
+++ plugins/importedDirectives.js
@@ -6,8 +6,12 @@ import { defineNuxtPlugin } from '#imports'
 import FloatingVue from 'floating-vue'
 import 'floating-vue/dist/style.css'
 import VueDragscroll from "vue-dragscroll";
+/*import VCalendar from 'v-calendar';
+import 'v-calendar/style.css';
+*/
 
 export default defineNuxtPlugin(({ vueApp }) => {
     vueApp.use(FloatingVue)
     vueApp.use(VueDragscroll);
+    //vueApp.use(VCalendar);
 })
