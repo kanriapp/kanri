cef4f7c00e08ab09badd5be57c5161dabce08d44
trobonox <57040351+trobonox@users.noreply.github.com>
2024-06-02 15:46:11 +0200

    feat: add tooltip direction as a prop

--- components/Tooltip.vue
+++ components/Tooltip.vue
@@ -5,7 +5,7 @@
                 <slot name="trigger"></slot>
             </TooltipTrigger>
             <TooltipPortal to=".default-layout">
-                <TooltipContent side="right" class="z-[900] border border-elevation-1 data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade select-none rounded-[4px] bg-primary-darker px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]">
+                <TooltipContent :side="direction" class="z-[900] border border-elevation-1 data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade select-none rounded-[4px] bg-primary-darker px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]">
                     <slot name="content"></slot>
                     <TooltipArrow class="fill-bg-primary-darker" :width="10" />
                 </TooltipContent>
@@ -13,3 +13,14 @@
         </TooltipRoot>
     </TooltipProvider>
 </template>
+
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{
+        direction?: "right" | "top" | "bottom" | "left";
+    }>(),
+    {
+        direction: "right"
+    }
+);
+</script>
