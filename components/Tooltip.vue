95ceae309fb4eff6a5eb6f6aef4774b8d36e1456
trobonox <57040351+trobonox@users.noreply.github.com>
2024-06-03 17:32:51 +0200

    fix: increase z-index for tooltip

--- components/Tooltip.vue
+++ components/Tooltip.vue
@@ -25,7 +25,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
                 <slot name="trigger"></slot>
             </TooltipTrigger>
             <TooltipPortal to=".default-layout">
-                <TooltipContent :side="direction" class="z-[900] border border-elevation-1 data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade select-none rounded-[4px] bg-primary-darker px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]">
+                <TooltipContent :side="direction" class="z-[999999999] border border-elevation-1 data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade select-none rounded-[4px] bg-primary-darker px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]">
                     <slot name="content"></slot>
                     <TooltipArrow class="fill-bg-primary-darker" :width="10" />
                 </TooltipContent>
