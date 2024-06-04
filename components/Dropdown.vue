d7cf810346fe76d9137a92c3e6288d803904c7f7
trobonox <57040351+trobonox@users.noreply.github.com>
2024-06-04 11:18:52 +0200

    feat: add props for dropdown options

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -6,8 +6,9 @@
 
     <DropdownMenuPortal to=".default-layout">
       <DropdownMenuContent
-        class="z-[99999] min-w-[220px] outline-none bg-primary-darker border border-elevation-1 rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
-        :side-offset="5"
+        align="end"
+        class="z-[99999] min-w-[100px] outline-none bg-primary-darker border border-elevation-1 rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+        :side-offset="sideOffset"
       >
         <slot name="content"></slot>
       </DropdownMenuContent>
@@ -22,4 +23,15 @@ import {
   DropdownMenuRoot,
   DropdownMenuTrigger,
 } from 'radix-vue'
+
+const props = withDefaults(
+    defineProps<{
+        align?: "start" | "center" | "end" | undefined
+        sideOffset?: number
+    }>(),
+    {
+        align: undefined,
+        sideOffset: 5
+    }
+);
 </script>
