b98659414d7dc9923dec9326cdf97276cb173ff4
trobonox <57040351+trobonox@users.noreply.github.com>
2024-06-04 10:40:13 +0200

    feat: add radix-vue dropdown

---
+++ components/Dropdown.vue
@@ -0,0 +1,25 @@
+<template>
+  <DropdownMenuRoot>
+    <DropdownMenuTrigger>
+        <slot name="trigger"></slot>
+    </DropdownMenuTrigger>
+
+    <DropdownMenuPortal to=".default-layout">
+      <DropdownMenuContent
+        class="z-[99999] min-w-[220px] outline-none bg-primary-darker border border-elevation-1 rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+        :side-offset="5"
+      >
+        <slot name="content"></slot>
+      </DropdownMenuContent>
+    </DropdownMenuPortal>
+  </DropdownMenuRoot>
+</template>
+
+<script setup lang="ts">
+import {
+  DropdownMenuContent,
+  DropdownMenuPortal,
+  DropdownMenuRoot,
+  DropdownMenuTrigger,
+} from 'radix-vue'
+</script>
