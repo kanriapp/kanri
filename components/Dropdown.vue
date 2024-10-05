ff7cf0834cbecc007554ac30a58e121fef243fdb
trobonox <57040351+trobonox@users.noreply.github.com>
2024-10-05 12:05:00 +0200

    fix: remove imports that break radix-vue dropdown

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -1,23 +1,3 @@
-<!-- SPDX-FileCopyrightText: Copyright (c) 2022-2024 trobonox <hello@trobo.dev> -->
-<!-- -->
-<!-- SPDX-License-Identifier: GPL-3.0-or-later -->
-<!--
-Kanri is an offline Kanban board app made using Tauri and Nuxt.
-Copyright (C) 2022-2024 trobonox <hello@trobo.dev>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
-
 <template>
     <DropdownMenuRoot>
         <DropdownMenuTrigger>
@@ -26,7 +6,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 
         <DropdownMenuPortal to=".default-layout">
             <DropdownMenuContent
-                align="end"
+                :align="align"
                 class="bg-primary-darker border-elevation-1 z-[99999] min-w-[100px] rounded-md border p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] outline-none will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade"
                 :side-offset="sideOffset"
             >
@@ -37,13 +17,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 </template>
 
 <script setup lang="ts">
-import {
-    DropdownMenuContent,
-    DropdownMenuPortal,
-    DropdownMenuRoot,
-    DropdownMenuTrigger,
-} from 'radix-vue'
-
 withDefaults(
     defineProps<{
         align?: "start" | "center" | "end" | undefined
