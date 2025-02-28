1fcf686adfa8d9f61ef127d4b740a4b4d24abcf6
trobonox <57040351+trobonox@users.noreply.github.com>
2025-02-28 20:14:42 +0100

    fix: add fs plugin on rust side

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,6 +15,7 @@ use tauri_plugin_autostart::MacosLauncher;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_window_state::Builder::default().build())
