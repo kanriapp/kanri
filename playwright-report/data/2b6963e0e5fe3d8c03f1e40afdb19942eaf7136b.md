02c56a29d613eec848bf7b7af32f02a3d6d900b5
devsupport-tech <devsupport@levelupbusiness.tech>
2025-07-29 13:12:41 -0500

    sp

---
+++ playwright-report/data/2b6963e0e5fe3d8c03f1e40afdb19942eaf7136b.md
@@ -0,0 +1,5 @@
+# Page snapshot
+
+```yaml
+- text: Loading dupes...
+```
--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests/e2e',
+  testDir: './',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
