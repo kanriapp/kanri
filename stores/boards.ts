3508366ead5c4d380d85bdc19bbe28d0e71346c8
trobonox <57040351+trobonox@users.noreply.github.com>
2025-11-10 11:14:27 +0100

    fix: reuse compliance

--- stores/boards.ts
+++ stores/boards.ts
@@ -1,3 +1,24 @@
+/* SPDX-FileCopyrightText: Copyright (c) 2022-2025 trobonox <hello@trobo.dev>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+
+Kanri is an offline Kanban board app made using Tauri and Nuxt.
+Copyright (C) 2022-2025 trobonox <hello@trobo.dev>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { defineStore } from "pinia";
 import type { Board, Column, Card, Tag } from "@/types/kanban-types";
 import { useTauriStore } from "@/stores/tauriStore";
